78392c54458dcd7b720c48f19e2c8a17e84a316a
Rajabrahmam-Thanniru <raja9392t@gmail.com>
2024-07-26 14:58:18 +0530

    .

--- front_end/src/components/pages/UploadNote.tsx
+++ front_end/src/components/pages/UploadNote.tsx
@@ -1,9 +1,15 @@
 import "../css/UploadNote.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 const UploadNote = () => {
   return (
-    <div>
-      <h1>Create Note</h1>
-    </div>
+    <>
+      <div>
+        <h1>Create Note</h1>
+      </div>
+      <div>
+        <Formik initialValues={}></Formik>
+      </div>
+    </>
   );
 };
 
